bebd306a54cec16b19fa69408dae23a0d8c50ca5
mubeka11 <munisebetulk@gmail.com>
2023-01-11 16:27:26 +0100

    Selenium-Day6

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractice.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import org.testng.annotations.Test;
+
+public class DropdownPractice {
+    @Test
+    public void dropdown_Task5(){
+
+    }
+
+
+}
